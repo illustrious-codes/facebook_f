bdc4f0418e6bc22c80b08a46b582d9c55051e601
illustrious-codes <onatayodavid105@gmail.com>
2024-07-25 13:38:24 +0100

    Facebook.com

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
-pip install -r requirements.txt
+python3 -m gunicorn blogconfiguration.wsgi:application
